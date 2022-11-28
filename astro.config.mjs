0f660e836d98267eeaff678c0683aa0a4c188bcc
nameindustry <nameindustry@gmail.com>
2022-11-28 15:14:18 -0500

    [ update ] - add in base path for maplePost on io

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://maplepost.github.io',
+  base: '/www-medimuse.net',
   integrations: [tailwind()]
 });
