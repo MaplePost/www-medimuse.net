d7817418f7837bccf97110f40f5bd04842e9dbfd
Alexander Davis <nameindustry@gmail.com>
2022-11-29 15:32:00 -0500

    [ update ] - Add in custom domain for building the pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://maplepost.github.io',
-  base: '/www-medimuse.net',
+  site: 'https://medimuse.net/',
   integrations: [tailwind()]
 });
